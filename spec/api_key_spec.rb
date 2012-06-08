c6a1c0155ef07624d1aeb80fde762c327fe75bfa
alexnotov <anotov@gmail.com>
2012-06-08 13:38:42 -0700

    [Finished #30934769] RSpec suite is now runnable with autotest
    Updated Balanced::Resource.construct_from_response to use symbol for
    has_key? check, since symbols auto-convert to strings.

--- spec/api_key_spec.rb
+++ spec/api_key_spec.rb
@@ -2,19 +2,46 @@ require "spec_helper"
 
 describe "ApiKey Resource" do
   before(:each) do
+    Balanced.stub(:send) { 
+      mock(
+        Faraday::Response, 
+        body: 
+          {
+            merchant: {
+              phone_number: "+16505551212", 
+              city: "Nowhere", 
+              marketplace: nil, 
+              name: "William Henry Cavendish III", 
+              email_address: "whc@example.org", 
+              created_at: "2012-06-08T01:51:36.489382Z", 
+              uri: "/v1/merchants/TEST-merchant_id", 
+              accounts_uri: "/v1/merchants/TEST-merchant_id/accounts", 
+              meta: {}, 
+              postal_code: "90210", 
+              country_code: "USA", 
+              type: "person", 
+              balance: 0, 
+              api_keys_uri: "/v1/merchants/TEST-merchant_id/api_keys", 
+              id: "TEST-merchant_id", 
+              street_address: "123 Fake St"
+            },
+            secret: "some-secret", 
+            meta: {},
+            uri: "/v1/api_keys/api-key",
+            id: "api-key"
+          }
+      )
+    }
   end
 
   it "should create my api key and let me access the secret" do
     key = Balanced::ApiKey.new.save
-    key.should_not eq(nil)
     # make sure my secret is there.
-    key.secret.should_not eq(nil)
+    key.secret.should == "some-secret"
   end
 
   it "should construct the merchant sub resource as an instance of Balanced::Merchant" do
     key = Balanced::ApiKey.new.save
     (key.merchant.instance_of? Balanced::Merchant).should == true
   end
-
-
 end
