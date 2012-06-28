e21a37667f20c05046a22fbb961d6d6ee1434d69
Alex Notov and David Grieser <team+alexnotov+davidgrieser@bizeebee.com>
2012-06-28 15:12:29 -0700

    Moved spec into proper location.

--- spec/balanced/resources/account_spec.rb
+++ spec/balanced/resources/account_spec.rb
@@ -419,4 +419,36 @@ describe Balanced::Account do
 
     end
   end
+
+  describe ".find_by_email" do
+    use_vcr_cassette
+    before do
+      api_key = Balanced::ApiKey.new.save
+      Balanced.configure api_key.secret
+      @marketplace = Balanced::Marketplace.new.save
+      card = Balanced::Card.new(
+        :card_number => "5105105105105100",
+        :expiration_month => "12",
+        :expiration_year => "2015",
+      ).save
+      buyer = Balanced::Marketplace.my_marketplace.create_buyer(
+        "john.doe@example.com",
+        card.uri
+      )
+    end
+
+    context "email address is in system" do
+      use_vcr_cassette
+      it "should return account object" do
+        Balanced::Account.find_by_email("john.doe@example.com").should be_instance_of Balanced::Account
+      end
+    end
+
+    context "email address does not exist" do
+      use_vcr_cassette
+      it "should return nil" do
+        Balanced::Account.find_by_email("foo@bar.com").should be_nil
+      end
+    end
+  end
 end
