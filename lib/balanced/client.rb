7e6bbceb8a5133d898657448552667213474f40e
Brian Moseley <bcm@maz.org>
2012-08-14 17:23:33 -0700

    fixes #28: allow the logger to be configured
    also respect the log level config rather than always using the default
    log level

--- lib/balanced/client.rb
+++ lib/balanced/client.rb
@@ -14,6 +14,7 @@ module Balanced
       :port => 5000,
       :version => '1',
       :logging_level => 'WARN',
+      :logger => nil
     }
 
     attr_reader :conn
@@ -26,8 +27,12 @@ module Balanced
     end
 
     def build_conn
-      logger = Logger.new(STDOUT)
-      logger.level = Logger.const_get(DEFAULTS[:logging_level].to_s)
+      if config[:logger]
+        logger = config[:logger]
+      else
+        logger = Logger.new(STDOUT)
+        logger.level = Logger.const_get(config[:logging_level].to_s)
+      end
 
       Faraday.register_middleware :response,
           :handle_balanced_errors => lambda {Faraday::Response::RaiseBalancedError}
