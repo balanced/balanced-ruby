ff0a9a98a9140d4834898470d43766ab8ad5d570
Alex Sharp <ajsharp@gmail.com>
2013-07-03 11:25:03 -0700

    spec ensuring that associated cards can be debited

--- spec/balanced/resources/card_spec.rb
+++ spec/balanced/resources/card_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Balanced::Card, '#debit', vcr: true, marketplace: true do
-  it 'raises an exception with an unassociated' do
+  it 'raises an exception with an unassociated card' do
     card = Balanced::Card.new
     expect {
       card.debit
@@ -10,10 +10,27 @@ describe Balanced::Card, '#debit', vcr: true, marketplace: true do
 end
 
 describe Balanced::Card, '#hold', vcr: true, marketplace: true do
-  it 'raises an exception with an unassociated' do
+  it 'raises an exception with an unassociated card' do
     card = Balanced::Card.new
     expect {
       card.hold
     }.to raise_error(Balanced::UnassociatedCardError)
+  end
+end
+
+describe Balanced::Card, '#debit', vcr: true, marketplace: true do
+  it 'debits the card if an account is set' do
+    # tokenize card
+    card = Balanced::Card.new(
+      card_number:      '4111111111111111',
+      expiration_year:  '2016',
+      expiration_month: '12')
+    card.save
+
+    # associate card to account
+    Balanced::Account.new(email_address: 'user@example.com', name: 'John Doe', card_uri: card.uri).save
+
+    card = Balanced::Card.find(card.uri)
+    card.debit(amount: 10000).should be_instance_of Balanced::Debit
   end
 end
