7af22acd47f218f1ee61448bfaa894fb9a726439
Ben Mills <ben@unfiniti.com>
2013-07-24 06:41:26 -0600

    Add no unique constraint on email_address note to customer.find_by_email

--- lib/balanced/resources/customer.rb
+++ lib/balanced/resources/customer.rb
@@ -21,6 +21,10 @@ module Balanced
 
     # Attempts to find an existing customer by email
     #
+    # *NOTE:* There is no unique constraint on email_address.
+    #         Multiple customers with the same email may exist.
+    #         Only one Customer is returned.
+    #
     # @param [String] email An email of a customer
     # @return [Customer] if customer is found
     # @return [nil] if customer is not found
