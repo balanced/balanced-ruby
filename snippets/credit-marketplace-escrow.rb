243631d1d73be2065595287b9c51776d73d38054
richie serna <richie@balancedpayments.com>
2014-08-19 20:39:23 -0700

    escrow snippets

---
+++ snippets/credit-marketplace-escrow.rb
@@ -0,0 +1,4 @@
+Balanced::Marketplace.mine.owner_customer.bank_accounts.first.credit(
+    :amount => 2000000,
+    :description => 'Credit from Balanced escrow'
+)
---
+++ snippets/debit-marketplace-escrow.rb
@@ -0,0 +1,4 @@
+Balanced::Marketplace.mine.owner_customer.bank_accounts.first.debit(
+    :amount => 2000000,
+    :description => 'Pre-fund Balanced escrow'
+)
